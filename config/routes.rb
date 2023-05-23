e2712f3d09ac8d7dd394710907b2c4ded9729461
doyouwantto2 <tinhphong2580@gmail.com>
2023-05-23 12:56:18 +0700

    Update 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  resources :users
+  #get 'home/index'
+  get 'home/about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root 'home#index'
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :users do
+    member do
+      delete :delete
+    end
+  end
 end
