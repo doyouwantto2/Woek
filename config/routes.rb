d9171c3b8f00e5f7ba6addc26effa988f85c28f2
doyouwantto2 <tinhphong2580@gmail.com>
2023-06-12 02:44:40 +0700

    Update 3

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   #get 'home/index'
   get 'home/about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'home/contact'
+  
   root 'home#index'
   # Defines the root path route ("/")
   # root "articles#index"
